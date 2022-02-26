bb9fa0254b531d850bfbdf238b33d96afcc38cb8
Kees van Voorthuizen <me@keesvv.nl>
2022-02-27 00:12:13 +0100

    feat: add ActionButton

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="action-button bg-blue-500 text-white text-sm font-bold py-2.5 px-3.5 rounded-md"
+  >
+    {{ caption }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { caption } = defineProps({
+  caption: { type: String, required: true },
+});
+</script>
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="jumbotron flex justify-between items-center bg-gray-100 p-16">
+  <div class="jumbotron flex justify-between items-center bg-gray-100 p-24">
     <div class="left">
       <h1 class="text-3xl mb-2">Hi there 👋</h1>
       <h2 class="text-5xl font-bold mb-4">My name is Kees</h2>
       <!-- <h3>Feel free to <a href="mailto:contact@keesvv.nl">contact me</a>.</h3> -->
       <!-- <Socials /> -->
       <AutoTyper
-        class="text-md"
+        class="text-md mb-6"
         :strings="[
           'I write software.',
           'I make websites.',
           'I build scalable backends.',
         ]"
       />
+      <ActionButton caption="Contact me" />
     </div>
     <div class="right">
       <img class="w-52 mb-6 rounded-full" src="../assets/me.jpg" alt="Me" />
@@ -22,4 +23,5 @@
 
 <script lang="ts" setup>
 import AutoTyper from "./AutoTyper.vue";
+import ActionButton from "./ActionButton.vue";
 </script>
