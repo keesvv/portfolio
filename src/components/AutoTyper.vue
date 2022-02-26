013bcfe942795273cbcae0f74743f1e27bb55290
Kees van Voorthuizen <me@keesvv.nl>
2022-02-26 23:56:43 +0100

    feat(auto-typer): continuously loop over array of strings

--- src/components/AutoTyper.vue
+++ src/components/AutoTyper.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="autotyper font-mono">
-    <span>{{ visible }}</span>
+    <span>{{ buf }}</span>
     <span class="ml-1">&#9608;</span>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, PropType } from "vue";
 
-const { text } = defineProps({
-  text: { type: String, required: true },
+const { strings } = defineProps({
+  strings: { type: Array as PropType<string[]>, required: true },
 });
 
-const visible = ref("");
+const buf = ref("");
 
 const sleep = (ms: number) =>
   new Promise<void>((resolve) => setTimeout(resolve, ms));
 
-onMounted(async () => {
-  for (const char of text) {
-    visible.value += char;
+const type = async (str: string) => {
+  for (const char of str) {
+    buf.value += char;
     await sleep(Math.floor(Math.random() * 30 + 20));
   }
-});
+};
 
-// const items = [
-//   "I write software.",
-//   "I make websites.",
-//   "I build scalable backends.",
-// ];
+const reset = () => {
+  buf.value = "";
+};
+
+onMounted(async () => {
+  while (true) {
+    for (const str of strings) {
+      await sleep(250);
+      type(str);
+
+      await sleep(2000);
+      reset();
+    }
+  }
+});
 </script>
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -5,7 +5,14 @@
       <h2 class="text-5xl font-bold mb-4">My name is Kees</h2>
       <!-- <h3>Feel free to <a href="mailto:contact@keesvv.nl">contact me</a>.</h3> -->
       <!-- <Socials /> -->
-      <AutoTyper class="text-md" text="I write software." />
+      <AutoTyper
+        class="text-md"
+        :strings="[
+          'I write software.',
+          'I make websites.',
+          'I build scalable backends.',
+        ]"
+      />
     </div>
     <div class="right">
       <img class="w-52 mb-6 rounded-full" src="../assets/me.jpg" alt="Me" />
