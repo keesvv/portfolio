d454023e9cf0f3f6defbb6491623bd9d2d0a72bd
Kees van Voorthuizen <me@keesvv.nl>
2022-06-19 19:10:15 +0200

    feat: add PGP fingerprint

---
+++ src/lib/Fingerprint.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let fprint: string;
+  export let pubkeyUri: string;
+</script>
+
+<a href={pubkeyUri} class="fingerprint">
+  <span>PGP fingerprint</span>
+  <span>{fprint}</span>
+</a>
+
+<style lang="scss">
+  .fingerprint {
+    color: gray;
+    display: inline-block;
+    font-family: monospace;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  span {
+    display: block;
+  }
+</style>
--- src/lib/sections/ContactSection.svelte
+++ src/lib/sections/ContactSection.svelte
@@ -1,7 +1,17 @@
+<script>
+  import Fingerprint from "../Fingerprint.svelte";
+</script>
+
 <section class="contact">
   <h1>Interested?</h1>
   <h2>Shoot me an email.</h2>
   <a href="mailto:contactme@keesvv.nl">Contact me</a>
+  <div class="fingerprint">
+    <Fingerprint
+      fprint="131E 12C7 087E 5767 43CB 6C26 EAF3 F4D4 EE63 05A9"
+      pubkeyUri="https://github.com/keesvv.gpg"
+    />
+  </div>
 </section>
 
 <style lang="scss">
@@ -27,5 +37,9 @@
     display: inline-block;
     padding: 10px;
     margin-top: 10px;
+  }
+
+  .fingerprint {
+    font-size: 1.1em;
   }
 </style>
