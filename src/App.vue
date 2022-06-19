f8b45843838fb240bcb3880f9d69799ca0b953ee
Kees van Voorthuizen <me@keesvv.nl>
2022-06-19 09:28:01 +0200

    chore: clean up for rewrite

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Jumbotron from "./components/Jumbotron.vue";
-import Navbar from "./components/Navbar.vue";
-</script>
-
-<template>
-  <Navbar />
-  <Jumbotron />
-</template>
--- src/components/ContactButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <a
-    href="mailto:contact@keesvv.nl"
-    class="contact-button bg-blue-500 text-white text-sm font-bold py-2.5 px-3.5 rounded-md"
-  >
-    Contact me
-  </a>
-</template>
