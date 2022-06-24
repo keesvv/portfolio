b207f7034dd86b692d39060b49913a2276e4179a
Kees van Voorthuizen <me@keesvv.nl>
2022-06-24 12:31:07 +0200

    feat(projects): implement ProjectCard in ProjectsSection

--- src/lib/sections/ProjectsSection.svelte
+++ src/lib/sections/ProjectsSection.svelte
@@ -1,11 +1,40 @@
 <script lang="ts">
-  import projectsImg from "../../assets/icons/projects.svg";
+  import type { Project } from "../types";
+  import ProjectCard from "../ProjectCard.svelte";
+
+  import tsImg from "../../assets/technologies/typescript.svg";
+  import nestImg from "../../assets/technologies/nestjs.svg";
+  import vueImg from "../../assets/technologies/vue.svg";
+
+  import tribecampImg from "../../assets/projects/tribecamp.svg";
+
+  const projects: Project[] = [
+    {
+      name: "Tribecamp",
+      description: `
+        What was formerly known as a social network for
+        International Baccalaureate students eventually
+        became a Dutch LVS (student tracking system) and
+        my greatest project yet.`,
+      url: "https://github.com/tribecamp",
+      technologies: [
+        { name: "TypeScript", img: tsImg },
+        { name: "NestJS", img: nestImg },
+        { name: "Vue", img: vueImg },
+      ],
+      logo: [tribecampImg, "Tribecamp"],
+    },
+  ];
 </script>
 
 <section id="projects">
   <h1>Projects</h1>
-  <h2>I'll list them here ASAP.</h2>
-  <img src={projectsImg} alt="Projects icon" />
+  <h2>Featured projects of mine.</h2>
+  <div class="projects">
+    {#each projects as project}
+      <ProjectCard {project} />
+    {/each}
+  </div>
 </section>
 
 <style lang="scss">
@@ -13,12 +42,10 @@
     text-align: center;
   }
 
-  img {
-    margin-top: 15px;
-    width: 24px;
-
-    @media (prefers-color-scheme: dark) {
-      filter: invert(100%);
-    }
+  .projects {
+    display: flex;
+    justify-content: center;
+    text-align: initial;
+    margin-top: 20px;
   }
 </style>
